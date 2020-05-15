e46c2a227bcd8ac2c04b8abba8e51ffb04a52607
Georgiy Demo <demenchuk.george@protonmail.com>
2020-05-15 19:03:48 +0300

    Фиксанули зависимости

--- setup.py
+++ setup.py
@@ -8,5 +8,9 @@ setup(name='fa_api',
       url='https://github.com/GeorgiyDemo/fa_api',
       keywords='python api wrapper ruz fa ru faru library апи питон пайтон обёртка библиотека',
       description='API wrapper for ruz.fa.ru',
-      packages=['fa_api']
+      packages=['fa_api'],
+      install_requires=[
+         'requests==2.23.0',
+         'fake-headers==1.0.2',
+      ]
       )
