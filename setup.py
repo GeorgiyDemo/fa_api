a6b721dc45815f84dba09db4e497ef9ad8febca2
Georgiy Demo <demenchuk.george@protonmail.com>
2020-05-19 19:25:32 +0300

    README под PyPI #2

--- setup.py
+++ setup.py
@@ -1,7 +1,12 @@
 from setuptools import setup
+from os import path
+
+curr_dir = path.abspath(path.dirname(__file__))
+with open(path.join(curr_dir, 'README.md'), encoding='utf-8') as f:
+   long_description = f.read()
 
 setup(name='fa_api',
-      version='0.1',
+      version='0.2',
       author='Georgiy Demenchuk',
       author_email='demenchuk.george@protonmail.com',
       license='GPLv3',
@@ -9,6 +14,8 @@ setup(name='fa_api',
       keywords='python api wrapper ruz fa ru faru library апи питон пайтон обёртка библиотека',
       description='API wrapper for ruz.fa.ru',
       packages=['fa_api'],
+      long_description=long_description,
+      long_description_content_type='text/markdown',
       install_requires=[
          'requests==2.23.0',
          'fake-headers==1.0.2',
