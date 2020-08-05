b42c9ceb636e1fa07f7adc31712f8640c2cfe075
Georgiy Demo <demenchuk.george@protonmail.com>
2020-08-06 02:28:43 +0300

    Black formated

--- fa_api/api.py
+++ fa_api/api.py
@@ -35,7 +35,10 @@ class FaAPI:
         if r.status_code == 200:
             return r.json()
         raise requests.exceptions.BaseHTTPError(
-            "[Ошибка] RUZ отдал код {}!\nURL: '{}'".format(r.status_code, self.base_url + sub_url))
+            "[Ошибка] RUZ отдал код {}!\nURL: '{}'".format(
+                r.status_code, self.base_url + sub_url
+            )
+        )
 
     def search_group(self, group_name: str) -> List:
         """Поиск группы по ее названию"""
@@ -43,14 +46,20 @@ class FaAPI:
         r = self.__request("/api/search?term={}&type=group".format(group_name))
         return r
 
-    def timetable_group(self, group_id: str, date_begin: date = None, date_end: date = None) -> List:
+    def timetable_group(
+        self, group_id: str, date_begin: date = None, date_end: date = None
+    ) -> List:
         """Отдает расписание группы по её id"""
 
         if date_begin is None or date_end is None:
             date_begin = self.__date_now()
             date_end = date_begin
 
-        r = self.__request("/api/schedule/group/{}?start={}&finish={}&lng=1".format(group_id, date_begin, date_end))
+        r = self.__request(
+            "/api/schedule/group/{}?start={}&finish={}&lng=1".format(
+                group_id, date_begin, date_end
+            )
+        )
         return r
 
     def search_teacher(self, teacher_name: str) -> List:
@@ -59,23 +68,9 @@ class FaAPI:
         r = self.__request("/api/search?term={}&type=person".format(teacher_name))
         return r
 
-    def timetable_teacher(self, teacher_id: str, date_begin: date = None, date_end: date = None) -> List:
-        """Отдает расписание преподавателя по его id"""
-
-        if date_begin is None or date_end is None:
-            date_begin = self.__date_now()
-            date_end = date_begin
-
-        r = self.__request("/api/schedule/person/{}?start={}&finish={}&lng=1".format(teacher_id, date_begin, date_end))
-        return r
-
-    def search_auditorium(self, auditorium_name: str) -> List:
-        """Поиск аудитории по её названию"""
-
-        r = self.__request("/api/search?term={}&type=auditorium".format(auditorium_name))
-        return r
-
-    def timetable_auditorium(self, auditorium_id: str, date_begin: date = None, date_end: date = None) -> List:
+    def timetable_teacher(
+        self, teacher_id: str, date_begin: date = None, date_end: date = None
+    ) -> List:
         """Отдает расписание преподавателя по его id"""
 
         if date_begin is None or date_end is None:
@@ -83,7 +78,34 @@ class FaAPI:
             date_end = date_begin
 
         r = self.__request(
-            "/api/schedule/auditorium/{}?start={}&finish={}&lng=1".format(auditorium_id, date_begin, date_end))
+            "/api/schedule/person/{}?start={}&finish={}&lng=1".format(
+                teacher_id, date_begin, date_end
+            )
+        )
+        return r
+
+    def search_auditorium(self, auditorium_name: str) -> List:
+        """Поиск аудитории по её названию"""
+
+        r = self.__request(
+            "/api/search?term={}&type=auditorium".format(auditorium_name)
+        )
+        return r
+
+    def timetable_auditorium(
+        self, auditorium_id: str, date_begin: date = None, date_end: date = None
+    ) -> List:
+        """Отдает расписание преподавателя по его id"""
+
+        if date_begin is None or date_end is None:
+            date_begin = self.__date_now()
+            date_end = date_begin
+
+        r = self.__request(
+            "/api/schedule/auditorium/{}?start={}&finish={}&lng=1".format(
+                auditorium_id, date_begin, date_end
+            )
+        )
         return r
 
     def search_building(self, building_name: str) -> List:
@@ -92,7 +114,9 @@ class FaAPI:
         r = self.__request("/api/search?term={}&type=building".format(building_name))
         return r
 
-    def timetable_building(self, building_id: str, date_begin: date = None, date_end: date = None) -> List:
+    def timetable_building(
+        self, building_id: str, date_begin: date = None, date_end: date = None
+    ) -> List:
         """Отдает расписание здания по его id"""
 
         if date_begin is None or date_end is None:
@@ -100,5 +124,8 @@ class FaAPI:
             date_end = date_begin
 
         r = self.__request(
-            "/api/schedule/building/{}?start={}&finish={}&lng=1".format(building_id, date_begin, date_end))
+            "/api/schedule/building/{}?start={}&finish={}&lng=1".format(
+                building_id, date_begin, date_end
+            )
+        )
         return r
